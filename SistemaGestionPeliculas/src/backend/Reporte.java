216a800018698b84ef3c12dc357b1a188ad23b69
CarlosElMejor <129225581+Carlinhos2912@users.noreply.github.com>
2024-03-14 13:06:40 -0500

    Update Reporte.java
    los metodos retornan Strings con los resultados

--- SistemaGestionPeliculas/src/backend/Reporte.java
+++ SistemaGestionPeliculas/src/backend/Reporte.java
@@ -11,24 +11,27 @@ public class Reporte {
      * Peliculas más vendidas
      * Películas ordenadas por numero de ventas
      */
-    public void generarReportePeliculas(GestorCompras gesCom, GestorPeliculas gesPel) {
-        
+    public String generarReportePeliculas(GestorCompras gesCom, GestorPeliculas gesPel) {
+        String resultado = "";
+        return resultado;
     }
 
     /**
      * Clientes con más compras
      * Clientes ordenados por numero de compras 
      */
-    public void generarReporteClientes(GestorClientes gesCli, GestorCompras gesCom) {
-        
+    public String generarReporteClientes(GestorClientes gesCli, GestorCompras gesCom) {
+        String resultado = "";
+        return resultado;
     }
 
     /**
      * Ventas por periodo de tiempo
      * Mes de compra organizado por numero de ventas
      */
-    public void generarReporteVentas(GestorCompras gesCom) {
-        
+    public String generarReporteVentas(GestorCompras gesCom) {
+        String resultado = "";
+        return resultado;
     }
 
 }
